6ff1f0ba0d25f87d34d77890797252d9572e601d
Kara <kara_himanshu@yahoo.com>
2021-09-05 13:53:07 +0530

    Added Sum of triangle logic

---
+++ app.js
@@ -0,0 +1,23 @@
+const inputs = document.querySelectorAll(".angle-input");
+const isTriangleBtn = document.querySelector("#is-triangle-btn");
+const outputEl = document.querySelector("#output");
+
+function calculateSumOfAngles(angle1, angle2, angle3) {
+  const sumOfAngles = angle1 + angle2 + angle3;
+  return sumOfAngles;
+}
+
+function isTriangle() {
+  const a1 = Number(inputs[0].value);
+  const a2 = Number(inputs[1].value);
+  const a3 = Number(inputs[2].value);
+  const sumOfAngles = calculateSumOfAngles(a1, a2, a3);
+
+  if (sumOfAngles === 180) {
+    console.log("Yay! This is a triangle");
+  } else {
+    console.log("dhatt teri ki, this isn't a triangle");
+  }
+}
+
+isTriangleBtn.addEventListener("click", isTriangle);
