dd9e1c0d2653e3bc2c09aa2a778377e857fabf3b
Kara <kara_himanshu@yahoo.com>
2021-09-05 14:31:53 +0530

    file name updated

--- isTriangle.js
+++ isTriangle.js
@@ -14,9 +14,9 @@ function isTriangle() {
   const sumOfAngles = calculateSumOfAngles(a1, a2, a3);
 
   if (sumOfAngles === 180) {
-    outputEl.innerHTML = "Yay! This is a triangle";
+    outputEl.innerText = "Yay! This is a triangle";
   } else {
-    outputEl.innerHTML = "dhatt teri ki, this isn't a triangle";
+    outputEl.innerText = "dhatt teri ki, this isn't a triangle";
   }
 }
 
