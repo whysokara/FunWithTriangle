92d6bb40a9f91313c7ef190855cb789b5711810c
Kara <kara_himanshu@yahoo.com>
2021-09-08 01:07:31 +0530

    Added Quiz html

---
+++ quiz.js
@@ -0,0 +1,9 @@
+const quizForm = document.querySelector(".quiz-form");
+const submitBtn = document.querySelector("#submitAnswerBtn");
+const output = document.querySelector("#output");
+
+const correctAnswers = ["90°", "right angled"];
+
+function calculateScore() {}
+
+submitBtn.addEventListener("click", calculateScore);
