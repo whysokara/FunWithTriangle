c6e2526ac1290b5ea3660dbc1677506ab501d5d6
Kara <kara_himanshu@yahoo.com>
2021-09-08 01:13:43 +0530

    Added quiz logic

--- quiz.js
+++ quiz.js
@@ -4,6 +4,19 @@ const output = document.querySelector("#output");
 
 const correctAnswers = ["90°", "right angled"];
 
-function calculateScore() {}
+function calculateScore() {
+  let score = 0;
+  let index = 0;
+
+  const formResults = new FormData(quizForm);
+  for (let value of formResults.values()) {
+    if (value === correctAnswers[index]) {
+      score = score + 1;
+    }
+    index = index + 1;
+  }
+  console.log(score);
+  output.innerText = `Your score is ${score}`;
+}
 
 submitBtn.addEventListener("click", calculateScore);
